db5985163e4fc314b8899c60c48b537682382f2e
Atheer <atheerwaled66@hotmail.se>
2024-04-24 18:43:19 +0200

    feat: add VGG2 2 block vgg layer netowork

---
+++ VGG2.py
@@ -0,0 +1,31 @@
+import torch
+from torch import nn
+
+class VGG2(nn.Module):
+	def __init__(self):
+		super().__init__()
+		self.model = nn.Sequential(
+			# block 1
+			nn.Conv2d(3, 32, kernel_size=3, padding=1),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(32, 32, kernel_size=3, padding=1),
+			nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=(2,2)),
+			# block 2
+			nn.Conv2d(32, 64, kernel_size=3, padding=1),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(64, 64, kernel_size=3, padding=1),
+			nn.ReLU(inplace=True),
+			nn.MaxPool2d(kernel_size=(2, 2)),
+		)
+		self.classifier = nn.Sequential(
+			nn.Linear(64 * 8 * 8, 128),
+			nn.ReLU(inplace=True),
+			nn.Linear(128, 10),
+		)
+
+	def forward(self, x):
+		x = self.model(x)
+		x = torch.flatten(x, 1)
+		x = self.classifier(x)
+		return x
