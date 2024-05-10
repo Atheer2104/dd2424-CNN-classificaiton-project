c27b14922a3f3662819f0c410eaa2d11c83134cf
Atheer <atheerwaled66@hotmail.se>
2024-05-10 22:28:06 +0200

    feat: add utils functions for loading data #14

---
+++ helpers/load_data_util.py
@@ -0,0 +1,43 @@
+from torchvision import datasets
+from torch.utils.data import DataLoader, Subset
+import random
+
+def load_CIFAR10_train_validation(batch_size, train_transformations, validation_set_size):
+	training_data = datasets.CIFAR10(
+		root="../root", train=True, download=True, transform=train_transformations
+	)
+
+	validation_set_inidices = random.sample(
+		range(len(training_data)), validation_set_size
+	)
+	
+	training_set_inidices = list(set([x for x in range(len(training_data))]) - set(validation_set_inidices))
+	assert(len(training_set_inidices) == len(training_data) - validation_set_size)
+
+	validation_set = Subset(training_data, validation_set_inidices)
+	training_set = Subset(training_data, training_set_inidices)
+
+	validation_set_loader = DataLoader(validation_set, batch_size, shuffle=True, num_workers=2)
+	training_set_loader = DataLoader(training_set, batch_size, shuffle=True, num_workers=2)
+ 
+	return validation_set_loader, training_set_loader
+
+def load_CIFAR10_train(batch_size, train_transformation):
+	# load dataset
+	training_data = datasets.CIFAR10(
+		"../root",
+		train=True,
+		download=True,
+		transform=train_transformation,
+	)
+ 
+	return DataLoader(training_data, batch_size, shuffle=True, num_workers=2)
+
+def load_CIFAR10_test(batch_size, test_transformations):
+	test_data = datasets.CIFAR10(
+		root="../root", train=False, download=True, transform=test_transformations
+	)
+ 
+	return DataLoader(
+		test_data, batch_size, shuffle=True, num_workers=2
+	)
